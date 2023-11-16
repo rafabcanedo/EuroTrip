909cd833e573025e61f1e78e0f399c92d7dcdb5b
rafabcanedo <rafaitecanedo@gmail.com>
2023-11-16 10:33:24 -0300

    new features

--- src/Pages/Home/Home.tsx
+++ src/Pages/Home/Home.tsx
@@ -1,7 +1,12 @@
+import { ButtonList } from '../../components/ButtonList/ButtonList'
+import { LoadScript, GoogleMap, Marker } from '@react-google-maps/api'
+import { mapStyles } from '../../mapStyles'
+
 export default function Home() {
   return (
     <div>
       <h1 className="text-lg text-red-500">Home</h1>
+      <ButtonList />
     </div>
   )
 }
